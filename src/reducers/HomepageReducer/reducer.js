0961f1f183794f6550380479f75d5cc74918048c
Aleksandar Z <aleksandroz.mail@gmail.com>
2018-07-11 17:13:21 +0200

    Updated reducer to retrieve facebook reviews

--- src/reducers/HomepageReducer/reducer.js
+++ src/reducers/HomepageReducer/reducer.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 import * as action_types from './constants';
 
 const initialState = {
-  profile: { first_name: 'Agent'}
+  profile: { first_name: 'Agent'},
+  facebook_reviews: []
 };
 
 function homepageReducer(state = initialState, action) {
   switch(action.type) {
     case action_types.GET_ACCOUNT_INFO: {
       return Object.assign({...state}, {
-        profile: action.data.profile
+        profile: action.data.profile,
+        facebook_reviews: action.data.facebook_reviews
       });
 
     }
