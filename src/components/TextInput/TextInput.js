9e88bd0eec3a033f0e6264a8c3323b294bcd96dd
Aleksandar Z <aleksandroz.mail@gmail.com>
2018-09-24 16:07:09 +0200

    Updated text input class

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -5,7 +5,7 @@ function TextInput(props) {
     <label style={{display: 'block'}}>
       { props.label }
         <div className="form-group">
-      <input name={props.name} type={props.type} value={props.value} onChange={props.onChange} placeholder={props.placeholder}/>
+          <input name={props.name} type={props.type} value={props.value} onChange={props.onChange} placeholder={props.placeholder} className={props.className}/>
       </div>
     </label>
   );
